a90e77ecb9d026f9a2b8d4815dc932788ddd38f4
Joachim Flatberg Stamnes <joachifs@student.hist.no>
2016-02-04 20:11:40 +0100

    2desktop

--- BitKing/BitKing/frmLauncher.cs
+++ BitKing/BitKing/frmLauncher.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BitKing
 {
-    public partial class Form1 : Form
+    public partial class frmLauncher : Form
     {
-        public Form1()
+        public frmLauncher()
         {
             InitializeComponent();
         }
